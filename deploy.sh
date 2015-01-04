957e61b55b01e3e105c95ecd1a05ed8bfb3ad5ad
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:07:28 -0500

    still testing

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 set -e
 git config user.name cboettig
 git config user.email cboettig@server.com
-
-if [ ! -d _posts  ]; then
-	git checkout gh-pages 
-fi
-rsync -a _site/ . 
-rm -rf _site && git add -A . && git commit -m 'Site updated' && git push
+cd .. && git clone https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
+cp -a _site/ deploy/ 
+cd deploy && git add -A . && git commit -m 'Site updated' && git push
