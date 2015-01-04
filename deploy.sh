4072338503dfd9ac384f579158786919309072dd
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:12:04 -0500

    fix git config

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-git config user.name cboettig
-git config user.email cboettig@server.com
+git config --global user.name cboettig
+git config --global user.email cboettig@server.com
 cd .. && git clone https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
 cp -a source/_site/ deploy/ 
 cd deploy && git add -A . && git commit -m 'Site updated' && git push
