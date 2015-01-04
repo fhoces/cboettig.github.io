ee5ebe1f313a3c4d578f9ecb8021723906263c4f
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:40:34 -0500

    avoid circular builds

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
-set -e
-git config --global user.name cboettig
-git config --global user.email cboettig@server.com
-cd .. && git clone https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
-cp -a source/_site/ deploy/ 
-cd deploy && git add -A . && git commit -m 'Site updated' && git push
+#!/bin/bash -i
+
+if [ "$DRONE_BRANCH" ]
+  then 
+    if [ "$DRONE_BRANCH" = "source" ]
+      then
+        sudo apt-get update && sudo apt-get install -y rsync
+        git config --global user.name cboettg
+        git config --global user.email cboettig@server.com
+	git config --global push.default simple
+        git clone -b source https://github.com/cboettig/cboettig.github.io ../source
+        cd ../source && Rscript -e 'servr::jekyll(serve=FALSE)'
+        cd .. && git clone -b master https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
+        rsync -av source/_site/ deploy/
+        cd deploy
+        git add -A . && git commit -m 'Site updated from drone' && git push
+    fi
+fi
