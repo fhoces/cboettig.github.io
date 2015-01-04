a37ae41efb2c9cb360f0745a629d8cc10c836121
Carl Boettiger <cboettig@gmail.com>
2015-01-04 01:10:48 -0500

    fix path

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ set -e
 git config user.name cboettig
 git config user.email cboettig@server.com
 cd .. && git clone https://cboettig:${GH_TOKEN}@github.com/cboettig/cboettig.github.io deploy 
-cp -a _site/ deploy/ 
+cp -a source/_site/ deploy/ 
 cd deploy && git add -A . && git commit -m 'Site updated' && git push
